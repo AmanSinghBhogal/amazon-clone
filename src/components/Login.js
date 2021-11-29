e6fae413a0e3cbd5392eb9fab65c875e684a91d7
Aman Singh Bhogal <bhogalamansingh22@gmail.com>
2021-11-29 19:51:52 +0530

    Login Page Front End completed

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,17 +11,35 @@ function Login()
                     <img src='/img/LoginPageIMg.png' alt='Oops! Image not Found.' />
                 </div>
             </Link>
+
+            {/* Login page Box */}
             <div className='loginpage__box'>
+
+                {/* Title */}
+                <h1>Sign-In</h1>
+
+                {/* Email Input */}
                 <p>
                     Email:
                 </p>
                 <input type='email'></input>
-                
+
+                {/* Password Input */}
                 <p>
                     Password: 
                 </p>
                 <input type='password'></input>
 
+                {/* Sign In Button */}
+                <button className='loginpage__signinbtn'>Sign in</button>
+
+                {/* Instructions */}
+                <p className='loginpage__instruction'>
+                    By continuing, you agree to Clone Amazon's Conditions of Use and Privacy Notice.
+                </p>
+
+                {/* Create Account Button */}
+                <button className='loginpage__createAccBtn'>Create Your Amazon Account</button>
             </div>
         </div>
     );
