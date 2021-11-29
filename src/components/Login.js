473076804df17f4fc53f82cf6eeb79c8f725cee4
Aman Singh Bhogal <bhogalamansingh22@gmail.com>
2021-11-29 16:47:22 +0530

    Login Page Front End 50% made

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Login.css';
 
 function Login()
 {
     return(
-        <div>
-            <p>Hello World</p>  
+        <div className='loginpage'>  
+            <Link to="/">
+                <div className='loginpage__logo'>
+                    <img src='/img/LoginPageIMg.png' alt='Oops! Image not Found.' />
+                </div>
+            </Link>
+            <div className='loginpage__box'>
+                <p>
+                    Email:
+                </p>
+                <input type='email'></input>
+                
+                <p>
+                    Password: 
+                </p>
+                <input type='password'></input>
+
+            </div>
         </div>
     );
 }
